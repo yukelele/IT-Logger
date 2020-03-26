042d34e7e21503d49491f888bfb808e9f6021b80
Yuki Mano <ysm@umail.ucsb.edu>
2020-03-26 16:12:47 -0700

    UI complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -9,7 +16,19 @@ function App() {
         // Init Materialize JS
         M.AutoInit();
     });
-    return <div className='App'>My app</div>;
+    return (
+        <Fragment>
+            <SearchBar />
+            <div className='container'>
+                <AddBtn />
+                <AddLogModal />
+                <EditLogModal />
+                <AddTechModal />
+                <TechListModal />
+                <Logs />
+            </div>
+        </Fragment>
+    );
 }
 
 export default App;
