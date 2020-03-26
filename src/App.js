a704452a92b2937db088db1cac64da4b57731c23
Yuki Mano <ysm@umail.ucsb.edu>
2020-03-26 13:02:36 -0700

    initliaze project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+
+function App() {
+    useEffect(() => {
+        // Init Materialize JS
+        M.AutoInit();
+    });
+    return <div className='App'>My app</div>;
+}
+
+export default App;
